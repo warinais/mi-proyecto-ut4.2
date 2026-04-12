583cca846c7ab061db53aa2f2ecb525328edd79c
PedroG <pedrogarrefuentes@gmail.com>
2026-04-12 11:04:08 +0200

    modificado proveedor para pedir sus datos

--- src/edut4/Proveedor.java
+++ src/edut4/Proveedor.java
@@ -1,5 +1,15 @@
 package edut4;
 
+import java.util.Scanner;
 public class Proveedor {
 
+    public static void pedirDatos(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nombre del proveedor");
+        String nombre = sc.nextLine();
+        System.out.println("Nombre del proveedor");
+        String cif = sc.nextLine();
+        System.out.println("Nombre del proveedor");
+        int telf = Integer.parseInt(sc.nextLine());
+    }
 }
